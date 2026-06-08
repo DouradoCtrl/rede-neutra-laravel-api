5f488162df49e3833563d9edb56c2142f41203c9
DouradoCtrl <gdouradoestudante@gmail.com>
2026-06-08 17:10:18 -0300

    feat(model): add multi-tenant trait, role, and relations to User

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,13 +9,23 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'telecom_group_id', 'role'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, \App\Traits\BelongsToTelecomGroup;
+
+    /**
+     * Get the telecom group that owns the user.
+     */
+    public function telecomGroup(): BelongsTo
+    {
+        return $this->belongsTo(TelecomGroup::class);
+    }
 
     /**
      * Get the attributes that should be cast.
