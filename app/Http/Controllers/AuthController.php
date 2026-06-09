28bb1848b2f4ecf584d4d7a534f79be1adf32c2b
DouradoCtrl <gdouradoestudante@gmail.com>
2026-06-09 11:39:55 -0300

    refactor(controllers): atualiza AuthController para consumir a AuthService

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,40 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use App\Models\User;
-use Illuminate\Validation\ValidationException;
+use App\Services\AuthService;
 
 class AuthController extends Controller
 {
+    public function __construct(private AuthService $authService)
+    {
+    }
+
     /**
      * Realiza o login e retorna o Token Sanctum (Bearer Token)
      */
     public function login(Request $request)
     {
-        $request->validate([    
-            'email' => ['required', 'email'],
-            'password' => ['required'],
-            'device_name' => ['string', 'nullable']
-        ]);
-
-        $user = User::where('email', $request->email)->first();
-
-        if (! $user || ! Hash::check($request->password, $user->password)) {
-            throw ValidationException::withMessages([
-                'email' => ['As credenciais fornecidas estão incorretas.'],
-            ]);
-        }
-
-        // Gera o token de acesso
-        $deviceName = $request->device_name ?? 'auth_token';
-        $token = $user->createToken($deviceName)->plainTextToken;
-
-        return response()->json([
-            'token' => $token,
-            'user' => $user->load('telecomGroup')
-        ]);
+        $result = $this->authService->login($request->all());
+        
+        return response()->json($result);
     }
 
     /**
@@ -44,8 +26,7 @@ class AuthController extends Controller
      */
     public function logout(Request $request)
     {
-        // Deleta o token que foi usado para autenticar esta requisição
-        $request->user()->currentAccessToken()->delete();
+        $this->authService->logout($request->user());
 
         return response()->json([
             'message' => 'Deslogado com sucesso'
