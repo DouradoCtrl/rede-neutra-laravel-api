0eb1104679d09e1a86e335406af61cd3569042a4
DouradoCtrl <gdouradoestudante@gmail.com>
2026-06-09 10:50:54 -0300

    style(auth): remove espaços em branco na validação de login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,7 +15,7 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
-        $request->validate([
+        $request->validate([    
             'email' => ['required', 'email'],
             'password' => ['required'],
             'device_name' => ['string', 'nullable']
